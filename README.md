a075db7189759aa020048de87a848c8f9a6ecc1e
Stephen H. Gerstacker <stephen@gerstacker.us>
2019-12-06 12:27:00 -0500

    Day 06, Part 2
    Added the PriorityQueue from last year to Utilities
    
    Added `dot` graph support.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Advent of Code 2019
 
 My submissions for [Advent of Code 2019](https://adventofcode.com)
+
+## Links
+
+* [Introduction to the A* Algorithm](https://www.redblobgames.com/pathfinding/a-star/introduction.html)
--- Sources/06/main.swift
+++ Sources/06/main.swift
@@ -34,6 +34,12 @@ class Object {
     }
 }
 
+extension Object: CustomStringConvertible {
+    var description: String {
+        return name
+    }
+}
+
 extension Object: Equatable {
     static func ==(lhs: Object, rhs: Object) -> Bool {
         return lhs.name == rhs.name
@@ -43,6 +49,52 @@ extension Object: Equatable {
 extension Object: Hashable {
     func hash(into hasher: inout Hasher) {
         hasher.combine(name)
+    }
+}
+
+func drawGraph(name: String, objects: [String:Object], highlights: [Object] = []) {
+    let desktopPath = NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true).first!
+    let outputPath = desktopPath + "/" + name
+
+    let arguments = ["-o", outputPath, "-Tpdf"]
+    execute(command: "/usr/local/bin/dot", arguments: arguments) { (inputPipe, outputPipe, errorPipe) in
+        let handle = inputPipe.fileHandleForWriting
+
+        handle.write(string: "digraph {\n")
+        handle.write(string: "  rankdir=\"RL\"\n")
+        handle.write(string: "  bgcolor=\"#2C2C2E\"\n")
+
+        handle.write(string: "  edge [color=\"#EBEBF5\"]\n")
+        handle.write(string: "  node [fontname=Helvetica, shape=circle, style=filled, color=white, fillcolor=\"#EBEBF5\"]\n")
+        handle.write(string: "  YOU [fillcolor=\"#FFD60A\"]\n")
+        handle.write(string: "  SAN [fillcolor=\"#FF453A\"]\n")
+
+        for highlight in highlights where highlight.name != "YOU" {
+            handle.write(string: "  \"\(highlight.name)\" [fillcolor=\"#30D158\"]\n")
+        }
+
+        for (_, object) in objects {
+            if let parent = object.parent {
+                handle.write(string: "  \"\(object.name)\" -> \"\(parent.name)\"\n")
+            }
+        }
+
+        handle.write(string: "}")
+        handle.closeFile()
+
+        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+        let outputString = String(bytes: outputData, encoding: .utf8)
+
+        if let output = outputString, !output.isEmpty {
+            print(output)
+        }
+
+        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+        let errorString = String(bytes: errorData, encoding: .utf8)
+
+        if let error = errorString, !error.isEmpty {
+            print(error)
+        }
     }
 }
 
@@ -90,39 +142,66 @@ let checksum = objects.reduce(0) { (sum, current) -> Int in
 
 print("Checksum: \(checksum)")
 
-let desktopPath = NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true).first!
-let outputPath = desktopPath + "/06-graph.pdf"
+drawGraph(name: "06-graph.pdf", objects: objects)
 
-let arguments = ["-o", outputPath, "-Tpdf"]
-execute(command: "/usr/local/bin/dot", arguments: arguments) { (inputPipe, outputPipe, errorPipe) in
-    let handle = inputPipe.fileHandleForWriting
+guard let youObject = objects["YOU"] else {
+    print("No YOU object. Exiting early")
+    exit(0)
+}
 
-    handle.write(string: "digraph {\n")
+guard let santaObject = objects["SAN"] else {
+    print("No SAN object. Exiting early")
+    exit(0)
+}
 
-    handle.write(string: "  node [fontname=Future]\n")
-    handle.write(string: "  YOU [shape=box, style=filled, color=yellow]\n")
-    handle.write(string: "  SAN [shape=diamond, style=filled, color=red]\n")
+// A* search for the shortest path
+var frontier = PriorityQueue<Object>()
+var cameFrom: [Object:Object] = [:]
+var costSoFar: [Object:Int] = [:]
 
-    for (_, object) in objects {
-        if let parent = object.parent {
-            handle.write(string: "  \"\(object.name)\" -> \"\(parent.name)\"\n")
+frontier.push(youObject, priority: 0)
+costSoFar[youObject] = 0
+
+while !frontier.isEmpty {
+    guard let current = frontier.pop() else {
+        print("Ran out of frontier to explore")
+        exit(1)
+    }
+
+    if current == santaObject {
+        break
+    }
+
+    var nextObjects = current.children
+
+    if let parent = current.parent {
+        nextObjects.insert(parent)
+    }
+
+    for nextObject in nextObjects {
+        let newCost = costSoFar[current]! + 1
+
+        if costSoFar[nextObject] == nil || newCost < costSoFar[nextObject]! {
+            costSoFar[nextObject] = newCost
+            frontier.push(nextObject, priority: newCost)
+            cameFrom[nextObject] = current
         }
     }
-
-    handle.write(string: "}")
-    handle.closeFile()
-
-    let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-    let outputString = String(bytes: outputData, encoding: .utf8)
-
-    if let output = outputString, !output.isEmpty {
-        print(output)
-    }
-
-    let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-    let errorString = String(bytes: errorData, encoding: .utf8)
-
-    if let error = errorString, !error.isEmpty {
-        print(error)
-    }
 }
+
+var current = santaObject
+var path: [Object] = []
+
+while current != youObject {
+    guard let nextObject = cameFrom[current] else {
+        fatalError("Broken path")
+    }
+
+    path.append(nextObject)
+    current = nextObject
+}
+
+print("Path: \(path)")
+print("Distance from YOU -> SAN: \(path.count - 2)") // Path contains YOU and YOU's current orbit
+
+drawGraph(name: "06-graph-path.pdf", objects: objects, highlights: path)
