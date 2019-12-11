f96d1af7343abc8c64330632a0e849517dae062a
Stephen H. Gerstacker <stephen@gerstacker.us>
2019-12-11 08:42:35 -0500

    Documented the Animator a bit

--- Sources/Utilities/Animator.swift
+++ Sources/Utilities/Animator.swift
@@ -11,6 +11,8 @@ import Foundation
 
 public class Animator {
 
+    public typealias DrawCallback = (CGContext) -> ()
+
     let width: Int
     let height: Int
     let frameRate: CMTime
@@ -21,6 +23,14 @@ public class Animator {
 
     var currentFrameTime = CMTime(seconds: 0.0, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
 
+    /**
+     Initialize a new Animator with the given dimensions and frame rate.
+
+     - Parameter width: The width of the canvas that is presented when drawing.
+     - Parameter height: The height of the canvas that is presented when drawing.
+     - Parameter frameRate: Supplied as a ratio, the frame rate the video will be produced at. For example, to achieve 30 FPS, you would supply `1.0 / 30.0`.
+     - Parameter url: The file URL to store the resulting video in.
+     */
     public init(width: Int, height: Int, frameRate: Double, url: URL) {
         precondition(width % 16 == 0)
         precondition(height % 16 == 0)
@@ -63,6 +73,15 @@ public class Animator {
         writer.startSession(atSourceTime: currentFrameTime)
     }
 
+    /**
+     Finalize the video file.
+
+     All submitted video frames will be drawn, encoded, and stored in the video file.
+
+     Adding new frames after calling complete is unsupported.
+
+     This call will block until the file writing has completed.
+     */
     public func complete() {
         writerInput.markAsFinished()
 
@@ -82,7 +101,14 @@ public class Animator {
         }
     }
 
-    public func draw(callback: (CGContext) -> ()) {
+    /**
+     Retreive a new `CGContext` to draw in.
+
+     Once the callback is complete, but provided context is submitted for encoding and writing.
+
+     - Parameter callback: The function that provides the `CGContext` to draw and submit a frame.
+     */
+    public func draw(callback: DrawCallback) {
         guard let pool = writerAdaptor.pixelBufferPool else {
             fatalError("No pixel buffer pool to pull from")
         }
