b815510b14017d3d3e314d38378032a4f8b3dcc4
Stephen H. Gerstacker <stephen@gerstacker.us>
2019-12-15 11:33:04 -0500

    Animation for Day 15

--- Sources/Day15/main.swift
+++ Sources/Day15/main.swift
@@ -37,6 +37,8 @@ enum Space {
 
 class Skutter {
 
+    static let blockSize = 20
+
     let computer: IntcodeComputer
 
     var currentPosition: Point
@@ -44,7 +46,16 @@ class Skutter {
     var unexplored: [Point]
     var oxygenSensorPosition: Point
 
-    init(program: [Int]) {
+    var minX = 0
+    var minY = 0
+    var maxX = 0
+    var maxY = 0
+
+    let animator: Animator?
+
+    var debugPrint = true
+
+    init(program: [Int], animator: Animator? = nil) {
         computer = IntcodeComputer(data: program, inputs: [])
 
         currentPosition = .zero
@@ -55,6 +66,65 @@ class Skutter {
 
         unexplored = []
         oxygenSensorPosition = .max
+
+        self.animator = animator
+    }
+
+    private func debug(_ message: String) {
+        guard debugPrint else {
+            return
+        }
+
+        print(message)
+    }
+
+    private func draw() {
+        guard let animator = animator else {
+            return
+        }
+
+        animator.draw { context in
+            let backgroundBounds = CGRect(x: 0, y: 0, width: context.width, height: context.height)
+            let backgroundColor = CGColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+
+            context.setFillColor(backgroundColor)
+            context.fill(backgroundBounds)
+
+            for (point, space) in map {
+                let spaceBounds = CGRect(
+                    x: (point.x - minX) * Skutter.blockSize,
+                    y: (point.y - minY) * Skutter.blockSize,
+                    width: Skutter.blockSize,
+                    height: Skutter.blockSize
+                )
+
+                let spaceColor: CGColor
+
+                switch space {
+                case .empty:
+                    spaceColor = CGColor(red: 0.92, green: 0.92, blue: 0.96, alpha: 1.0)
+                case .oxygen:
+                    spaceColor = CGColor(red: 0.39, green: 0.82, blue: 1.00, alpha: 1.0)
+                case .wall:
+                    spaceColor = CGColor(red: 0.33, green: 0.33, blue: 0.35, alpha: 1.0)
+                }
+
+                context.setFillColor(spaceColor)
+                context.fill(spaceBounds)
+            }
+
+            let skutterBounds = CGRect(
+                x: (currentPosition.x - minX) * Skutter.blockSize,
+                y: (currentPosition.y - minY) * Skutter.blockSize,
+                width: Skutter.blockSize,
+                height: Skutter.blockSize)
+                .insetBy(dx: CGFloat(Skutter.blockSize / 4), dy: CGFloat(Skutter.blockSize / 4))
+
+            let skutterColor = CGColor(red: 0.75, green: 0.35, blue: 0.95, alpha: 1.0)
+
+            context.setFillColor(skutterColor)
+            context.fill(skutterBounds)
+        }
     }
 
     func path(from fromPoint: Point, to point: Point) -> [Direction] {
@@ -129,13 +199,15 @@ class Skutter {
     func run() {
         unexplored = [currentPosition]
 
+        draw()
+
         while !unexplored.isEmpty {
             let exploringPoint = unexplored.removeFirst()
-            print("Next point to explore: \(exploringPoint)")
+            debug("Next point to explore: \(exploringPoint)")
 
             // Move to the exploring point from the current point
             var directions = path(to: exploringPoint)
-            print("Movement from \(currentPosition) to \(exploringPoint): \(directions)")
+            debug("Movement from \(currentPosition) to \(exploringPoint): \(directions)")
 
             while !directions.isEmpty {
                 let direction = directions.removeFirst()
@@ -171,7 +243,9 @@ class Skutter {
                     currentPosition = Point(x: currentPosition.x - 1, y: currentPosition.y)
                 }
 
-                print("-   Moved \(direction) to \(currentPosition)")
+                debug("-   Moved \(direction) to \(currentPosition)")
+
+                draw()
             }
 
             // Try each direction and add empty spaces to the frontier
@@ -196,7 +270,7 @@ class Skutter {
                     continue
                 }
 
-                print("Exploring \(direction)")
+                debug("Exploring \(direction)")
 
                 switch direction {
                 case .north:
@@ -220,22 +294,27 @@ class Skutter {
 
                 switch output {
                 case 0:
-                    print("-   Hit a wall")
+                    debug("-   Hit a wall")
                     shouldReturn = false
                     map[nextPoint] = .wall
                 case 1:
-                    print("-   Empty space")
+                    debug("-   Empty space")
                     shouldReturn = true
-                    unexplored.append(nextPoint)
+                    unexplored.insert(nextPoint, at: 0)
                     map[nextPoint] = .empty
                 case 2:
-                    print("-   Oxygen system!")
+                    debug("-   Oxygen system!")
                     shouldReturn = true
                     oxygenSensorPosition = nextPoint
                     map[nextPoint] = .oxygen
                 default:
                     fatalError("Unexpected output movement")
                 }
+
+                minX = min(minX, nextPoint.x)
+                minY = min(minY, nextPoint.y)
+                maxX = max(maxX, nextPoint.x)
+                maxY = max(maxY, nextPoint.y)
 
                 if shouldReturn {
                     switch direction {
@@ -258,12 +337,19 @@ class Skutter {
 
                     assert(output != 0)
                 }
+
+                draw()
             }
         }
+
+        animator?.complete()
     }
 }
 
+// MARK: - Part 1
+
 let data = Data.input
+let animate = true
 
 let skutter = Skutter(program: data)
 skutter.run()
@@ -273,3 +359,22 @@ print("Oxygen system at \(skutter.oxygenSensorPosition)")
 let path = skutter.path(from: .zero, to: skutter.oxygenSensorPosition)
 print("Path: \(path)")
 print("Distance: \(path.count)")
+
+// MARK: - Animate Part 1
+let animator: Animator?
+
+if animate {
+    let width = skutter.maxX - skutter.minX + 1
+    let height = skutter.maxY - skutter.minY + 1
+
+    let url = try! FileManager.default.url(for: .desktopDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+    let saveUrl = url.appendingPathComponent("15.mov")
+
+    animator = Animator(width: width * Skutter.blockSize, height: height * Skutter.blockSize, frameRate: 1.0 / 30.0, url: saveUrl)
+
+    let skutter = Skutter(program: data, animator: animator)
+    skutter.debugPrint = false
+    skutter.run()
+} else {
+    animator = nil
+}
